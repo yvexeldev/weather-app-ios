07e5e6e8329f5eaf82d9ae256460a07ca4291e5d
yvexeldev <azamabdusalomov@icloud.com>
2024-02-22 11:42:47 +0500

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Azam Abdusalomov on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
